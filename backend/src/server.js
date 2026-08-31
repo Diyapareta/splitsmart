b9a7eb2f1bd2010cce63ad998e2ddf46297599e1
Diyapareta <23bsm024@iiitdmj.ac.in>
2026-08-31 15:09:44 +0530

    fix in server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,13 +40,21 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log("MongoDB Connected");
+// DB + Server
 
-  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT,"0.0.0.0", () =>
-    console.log(`Server running on port ${PORT}`)
-  );
-})
-.catch((err) => {
-  console.error("MongoDB error:", err);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    family: 4
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB error:", err);
+  });
